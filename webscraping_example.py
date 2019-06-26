cfcc554b2086f4f2568e72215875b10d9a78aa59
Dheeraj Singh <bond.deejay@gmail.com>
2019-06-26 11:09:32 +0530

    Update webscraping_example.py

--- webscraping_example.py
+++ webscraping_example.py
@@ -1,3 +1,10 @@
+#*•	1st import: Allows you to launch/initialise a browser.
+#*•	2nd import: Allows you to search for things using specific parameters.
+#*•	3rd import: Allows you to wait for a page to load.
+#*•	4th import: Specify what you are looking for on a specific page
+# in order to determine that the webpage has loaded.
+#*•	5th import: Handling a timeout situation
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -9,20 +16,24 @@ option = webdriver.ChromeOptions()
 option.add_argument("--incognito")
 
 # Create new Instance of Chrome in incognito mode
-browser = webdriver.Chrome(executable_path='/Library/Application Support/Google/chromedriver', chrome_options=option)
+
+driverPath=r'chromedriver.exe'
+
+#browser = webdriver.Chrome(executable_path='/Library/Application Support/Google/chromedriver', chrome_options=option)
+browser = webdriver.Chrome(executable_path=driverPath, chrome_options=option)
 
 # Go to desired website
-browser.get("https://github.com/TheDancerCodes")
+browser.get("https://github.com/dheeraj-thedev")
 
 # Wait 20 seconds for page to load
-timeout = 20
+timeout = 40
 try:
     # Wait until the final element [Avatar link] is loaded.
     # Assumption: If Avatar link is loaded, the whole page would be relatively loaded because it is among
     # the last things to be loaded.
-    WebDriverWait(browser, timeout).until(EC.visibility_of_element_located((By.XPATH, "//img[@class='avatar width-full rounded-2']")))
-except TimeoutException:
-    print("Timed out waiting for page to load")
+    WebDriverWait(browser, timeout).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="js-pjax-container"]')))
+except TimeoutException as toe:
+    print("Timed out waiting for page to load {}".format(toe))
     browser.quit()
 
 # Get all of the titles for the pinned repositories
@@ -34,6 +45,9 @@ titles_element = browser.find_elements_by_xpath("//a[@class='text-bold']")
 
 # List Comprehension to get the actual repo titles and not the selenium objects.
 titles = [x.text for x in titles_element]
+
+#for x in titles_element:
+ #   print(x.text)
 
 # print response in terminal
 print('TITLES:')
